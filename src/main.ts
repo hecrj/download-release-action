e6fe5d45b5b3669e23681c2e621dfb7e34111e8f
Héctor Ramón Jiménez <hector0193@gmail.com>
2020-08-12 01:56:01 +0200

    Download release assets

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
-import * as core from '@actions/core';
 import * as path from 'path';
+import * as stream from 'stream';
+import * as fs from 'fs';
+import * as core from '@actions/core';
+import * as io from '@actions/io';
+import got from 'got';
 import { Octokit } from '@octokit/rest';
+import { promisify } from "util";
+
+const pipeline = promisify(stream.pipeline);
 
 async function run() {
   try {
     const repository = core.getInput('repository');
     const release = core.getInput('release');
+    const output = core.getInput('output');
     const token = core.getInput('token');
 
     const octokit = new Octokit({
@@ -20,7 +28,26 @@ async function run() {
       release_id: +release,
     });
 
-    core.info(JSON.stringify(assets));
+    core.info(JSON.stringify(assets, null, 2));
+
+    io.mkdirP(output);
+
+    for (let asset of assets.data) {
+      pipeline(
+        got.stream(
+          `https://api.github.com/repos/${repository}/releases/assets/${asset.id}`,
+          {
+            method: "GET",
+            headers: {
+              Accept: "application/octet-stream",
+              Authorization: `token ${token}`,
+              "User-Agent": "",
+            },
+          }
+        ),
+        fs.createWriteStream(`${output}/${asset.name}`)
+      );
+    }
 
   } catch (error) {
     core.setFailed(error.message);
