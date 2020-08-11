ad0529ba4571e2c8175dada043536810f50800c0
Héctor Ramón Jiménez <hector0193@gmail.com>
2020-08-12 01:39:08 +0200

    List the release assets

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
 import * as core from '@actions/core';
-import * as exec from '@actions/exec';
 import * as path from 'path';
+import { Octokit } from '@octokit/rest';
 
 async function run() {
   try {
     const repository = core.getInput('repository');
-    const components = core.getInput('release');
+    const release = core.getInput('release');
     const token = core.getInput('token');
+
+    const octokit = new Octokit({
+      auth: token,
+    });
+
+    const owner_repository = repository.split('/');
+
+    const assets = await octokit.repos.listReleaseAssets({
+      owner: owner_repository[0],
+      repo: owner_repository[1],
+      release_id: +release,
+    });
+
+    core.info(JSON.stringify(assets));
 
   } catch (error) {
     core.setFailed(error.message);
